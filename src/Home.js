6bea59edd1d2564ee0e3e1a12fc0e8df5c28e094
Santosh7017 <santoshmorya400@gmail.com>
2023-10-31 15:41:02 +0530

    task

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './component/footer'
+// import Carousel from './component/carousel'
+import Banners from './component/Banner'
+
+const Home = () => {
+  return (
+    <>
+    <Banners/>
+    {/* <Carousel /> */}
+    <Footer />
+    </>
+  )
+}
+
+export default Home
